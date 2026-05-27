99689301400fc61c3a8797b680a0e2f724e9810d
ayushxx01 <ayushxx01s@gmail.com>
2026-05-27 23:39:45 +0530

    implemented base screen

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { auth, db } from "../firebase/firebase";
+import { useNavigate } from 'react-router-dom';
+import {
+  doc,
+  getDoc
+} from "firebase/firestore";
 const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
+    const user = auth.currentUser;
+    const navigate = useNavigate();
+    const[formData, setFormData] = useState(null);
+    
+    useEffect(() => {const fetchUserData = async () => {
+        
+            if(!user) {
+
+            }
+
+            const docRef = doc(db,"users",user.uid);
+
+            const docSnap = await getDoc(docRef);
+
+            console.log(docSnap);
+
+            if(docSnap.exists()){
+                setFormData(docSnap.data());
+                
+            }
+            else{
+                console.log("No data")
+            }
+
+        
+    }
+fetchUserData()},[]);
+ if (!formData) {
+    return <h1>Loading...</h1>;
+  }
+    return (
+        <>
+        {
+            formData.isConnected ? (
+                 <h1>
+              Show Journal Screen
+            </h1>
+            ) : (
+                <>
+              <h1>
+                Connect With Partner
+              </h1>
+
+              <p>
+                Your Code:
+                {formData.partnerCode}
+              </p>
+
+              <button>
+                Copy Code
+              </button>
+
+              <input
+                placeholder="Enter Partner Code"
+              />
+
+              <button>
+                Connect
+              </button>
+            </>
+            ) 
+        }
+        </>
+    )
+
 }
 
 export default HomePage
