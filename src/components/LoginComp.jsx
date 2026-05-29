4ccce17ebbab972dbf92eb9cd13419ffcc24391c
ayushxx01 <ayushxx01s@gmail.com>
2026-05-29 17:46:24 +0530

    fixed flow of setprofile

--- src/components/LoginComp.jsx
+++ src/components/LoginComp.jsx
@@ -3,22 +3,34 @@ import { auth, db } from "../firebase/firebase";
 import {useState} from 'react'
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
-
+import { doc, getDoc } from "firebase/firestore";
 
 const LoginComp = () => {
      const[email, setEmail] = useState("");
     const[password, setPassword] = useState("");
+
     const navigate = useNavigate();
     const handleLogIn = async() =>{
         try {
             const userCredential = await signInWithEmailAndPassword(auth, email, password);
+                const user = userCredential.user;
+                const docRef = doc(db,'users',user.uid);
+                const docSnap = await getDoc(docRef);
+                console.log(userCredential.user);
 
-            console.log(userCredential.user);
-            navigate('/profile-setup');
+            if(docSnap.exists()){
+              navigate('/home');
+              
+            }
+            else{
+              navigate('/profile-setup')
+            }
+                  
         }
         catch (err){
             console.error(err);
         }
+        
     }
   return (
     <>
